ee8c31978692ab5cf5cb1cdab5e4d716880a1e25
jc3m <jonathanj.xu@yahoo.com>
2016-08-01 08:33:52 -0500

    Basics working, reading json/handlebars

---
+++ routes/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+
+var path = require('path');
+var fs = require('fs');
+var Handlebars = require('handlebars');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/read', function(req, res, next) {
+  if (!req.query.path) {
+    return next({
+      status: 400,
+      message: 'Must specify a path in query'
+    });
+  }
+
+  parseJSON(req.query.path, function(err, doc) {
+    if (err)
+      return next(err);
+    res.json(doc);
+  });
+});
+
+router.get('/resume', function(req, res, next) {
+  if (!req.query.path) {
+    return next({
+      status: 400,
+      message: 'Must specify a path in query'
+    });
+  }
+
+  parseJSON(req.query.path, function(err, doc) {
+    if (err)
+      return next(err);
+    render('t1.html', doc, function(err, resume) {
+      if (err)
+        return next(err);
+      res.send(resume);
+    });
+  });
+});
+
+function parseJSON(f, callback) {
+  var fp = path.join(__dirname, '..', 'resumes', f);
+  fs.readFile(fp, function(err, file) {
+    if (err)
+      return callback(err);
+    var out;
+    try {
+      out = JSON.parse(file);
+    } catch(err) {
+      return callback(err)
+    }
+    return callback(null, out);
+  });
+}
+
+function render(t, doc, callback) {
+  var templatePath = path.join(__dirname, '..', 'templates', t);
+  fs.readFile(templatePath, 'utf-8', function(err, temp) {
+    if (err)
+      return callback(err);
+    var template = Handlebars.compile(temp);
+    var res = template(doc);
+    console.log(res);
+    return callback(null, res);
+  });
+}
+
+module.exports = router;
